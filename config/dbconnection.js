81c960cb81bea75adcc585cd83418b71e27a2c9f
Rasi K P <rasir239@gmail.com>
2024-01-04 00:07:38 +0530

    update

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,5 +1,7 @@
 const mongoose=require('mongoose');
+const logger=require('../util/winston')
 require('dotenv').config();
 const connect= mongoose.connect(process.env.DATABASE_URL)
 .then(()=>console.log("connection sucessfully"))
+.then(()=>logger.info('connection sucess.'))
 .catch((err)=> console.error(err));
